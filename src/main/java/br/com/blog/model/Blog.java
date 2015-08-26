253c39089d9accc11db5ed44a6e946b0b65573dd
Victor Freitas <victormotuca@hotmail.com>
2015-08-26 13:59:13 -0300

    Projeto Cadastro Blog com comentario

---
+++ src/main/java/br/com/blog/model/Blog.java
@@ -0,0 +1,63 @@
+package br.com.blog.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Blog")
+public class Blog implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Blog(){
+		
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "site")
+	private String site;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSite() {
+		return site;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+	
+
+	
+	
+	
+}
